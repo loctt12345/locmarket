2877d82053473d95d4943103d5a1629bb7f468e8
loctt12345 <trnthinlc@gmail.com>
2023-05-04 16:46:13 +0700

    change to HashRouter

--- shopping-react/src/App.js
+++ shopping-react/src/App.js
@@ -1,5 +1,5 @@
 import Home from "./pages/home/Home";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {HashRouter as Router, Routes, Route} from "react-router-dom";
 import Product from "./pages/product/Product";
 import { useEffect, useState } from 'react';
 import { CategoryContext } from './context/CategoryContext';
